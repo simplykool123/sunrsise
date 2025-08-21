c1256eaa54fe9e74d02d67b08d65ab4423622b9e
simplykool123 <simplykool@gmail.com>
2025-08-22 00:24:57 +0530

    Fix Python socket module error by switching to Node.js server

---
+++ server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const port = 3000;
+
+// MIME types for different file extensions
+const mimeTypes = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+  '.woff': 'font/woff',
+  '.woff2': 'font/woff2',
+  '.ttf': 'font/ttf',
+  '.eot': 'application/vnd.ms-fontobject'
+};
+
+const server = http.createServer((req, res) => {
+  let pathname = url.parse(req.url).pathname;
+  
+  // Default to index.html for root path
+  if (pathname === '/') {
+    pathname = '/index.html';
+  }
+  
+  const filePath = path.join(__dirname, pathname);
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || 'text/plain';
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>');
+      } else {
+        res.writeHead(500, { 'Content-Type': 'text/html' });
+        res.end('<h1>500 Internal Server Error</h1>');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
